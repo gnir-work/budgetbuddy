ac83e9c430acbfba78c88ca9cacfd1fadd906164
Nir Geller <gnir.work@gmail.com>
2022-05-22 00:20:37 +0300

    Ran format.

--- credit_crawlers/src/isracard.py
+++ credit_crawlers/src/isracard.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import Dict, Union
 
 from splunk_api import send_json
@@ -15,9 +14,7 @@ DATE_FORMAT = "%d/%m/%Y"
     "--csv_file_path",
     help="The path to the csv file downloaded from isracard site.",
 )
-@click.option(
-    "-t", "--token", help="The HEC (HTTP Event Collector) from your splunk instance"
-)
+@click.option("-t", "--token", help="The HEC (HTTP Event Collector) from your splunk instance")
 def upload_csv_to_splunk(csv_file_path: str, token: str):
     with open(csv_file_path, encoding="utf-8") as csvf:
         csv_reader = csv.DictReader(csvf)
@@ -28,20 +25,16 @@ def upload_csv_to_splunk(csv_file_path: str, token: str):
                 send_json(data["data"], data["timestamp"], token)
 
 
-def _parse_row(row: Dict[str, str]) -> dict[str, Union[str,dict, int]]:
+def _parse_row(row: Dict[str, str]) -> dict[str, Union[str, dict, float]]:
     if len(row.get("תאריך רכישה", "").replace(" ", "")) > 1:
         return {
             "data": {
                 "Price": row["סכום חיוב"],
-                "Business": row["שם בית עסק"]
-                    .replace('"', "")
-                    .replace("'", "")
-                    .replace(" ", "_"),
+                "Business": row["שם בית עסק"].replace('"', "").replace("'", "").replace(" ", "_"),
                 "Date": row["תאריך רכישה"],
             },
-            "timestamp": datetime.datetime.strptime(row["תאריך רכישה"], DATE_FORMAT).timestamp()
+            "timestamp": datetime.datetime.strptime(row["תאריך רכישה"], DATE_FORMAT).timestamp(),
         }
-
 
     else:
         return {}
